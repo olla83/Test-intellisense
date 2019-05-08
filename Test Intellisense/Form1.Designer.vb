f3ed8ca187b0b8a00195201113d914df4543ce16
Oliver Lang <oliver.lang83@gmail.com>
2019-05-08 17:03:55 +0200

    ComboBox

--- Test Intellisense/Form1.Designer.vb
+++ Test Intellisense/Form1.Designer.vb
@@ -22,10 +22,55 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(272, 127)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(183, 182)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Wann ist Pause?"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(524, 127)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(175, 182)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Was macht Daniel in seiner pause?"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Alter", "Name", "Geschlecht - es geht schlecht", "Test"})
+        Me.ComboBox1.Location = New System.Drawing.Point(30, 127)
+        Me.ComboBox1.MaxLength = 20
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(236, 24)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- Test Intellisense/Form1.vb
+++ Test Intellisense/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Public Property ComboCustomerGroup As Object
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Es ist " & Now.ToShortTimeString & " und Zeit für eine Pause")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Daniel trinnkt ein Bier!")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Function GetCustomerGroupNameAndCode() As DataTable
+        Throw New NotImplementedException()
+    End Function
 End Class
